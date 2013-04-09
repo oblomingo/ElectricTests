c1be4347b0c11ac0516739d459e8fac55de41987
oblomingo <oblomingo@gmail.com>
2013-04-09 16:10:11 +0300

    Show menu as widget, mark current button

---
+++ ElectricTests/Controllers/MenuController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectricTests.Controllers
+{
+    public class MenuController : Controller
+    {
+        /// <summary>
+        /// Show menu as widget, mark button with current controller
+        /// </summary>
+        /// <param name="controllerName"></param>
+        /// <returns></returns>
+        [ChildActionOnly]
+        public ActionResult Show (string controllerName = null) {
+            ViewBag.ControllerName = controllerName;
+            
+            //All menu buttons names with their controller names
+            Dictionary<string, string> menu = new Dictionary<string, string>();
+            menu.Add("Pagrindinis", "Home");
+            menu.Add("Dokumentai", "Documents");
+            menu.Add("Testavimas", "Tests");
+            menu.Add("Pagalba", "Help");
+            
+            return PartialView(menu);
+        }
+    }
+}
